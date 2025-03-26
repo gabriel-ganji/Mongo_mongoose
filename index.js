2aa85ff1235be162f75d9970edf6a8c6ada7516c
Gabriel <gabrielganji01011010@gmail.com>
2025-03-26 17:48:41 -0300

    Saving data

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const mongoose = require("mongoose");
+const articleModel = require("./article");
 
 mongoose.connect("mongodb://localhost:27017/learningMongo");
+
+const Article = mongoose.model("Article", articleModel);
+
+const newArticle = new Article({title: "O homem mais rico da babilônia", author: "George Samuel Clason", body: "..."});
+
+newArticle.save().then(() => {
+    console.log("Artigo salvo!");
+}).catch(error => {
+    console.log(error);
+});
