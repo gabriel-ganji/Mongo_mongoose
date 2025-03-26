3cc3f245bdf4f62709008f2f515fe3f66a5bdf9c
Gabriel <gabrielganji01011010@gmail.com>
2025-03-26 18:05:51 -0300

    Data search

--- index.js
+++ index.js
@@ -5,7 +5,28 @@ mongoose.connect("mongodb://localhost:27017/learningMongo");
 
 const Article = mongoose.model("Article", articleModel);
 
-const newArticle = new Article({title: "O homem mais rico da babilônia", author: "George Samuel Clason", body: "..."});
+Article.find({'_id': "67e468eff300c33886edb8e2"}).then(articles => {
+    console.log(articles);
+}).catch(error => {
+    console.log(error);
+});
+
+// Article.find({}).then(articles => {
+//     console.log(articles);
+// }).catch(error => {
+//     console.log(error);
+// });
+
+const newArticle = new Article({
+    title: "O homem mais rico da babilônia", 
+    author: "George Samuel Clason", 
+    special: true,
+    body: "...body...",
+    resume: {
+        content: "...resume content...",
+        author: "Author of resume"
+    }
+});
 
 newArticle.save().then(() => {
     console.log("Artigo salvo!");
